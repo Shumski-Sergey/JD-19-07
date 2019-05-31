ece8cb35626914ada981c2114ff628f67d9e7616
dmbelov <dmbelov777@gmail.com>
2019-05-31 21:55:19 +0300

    Lesson2 Task B1 changed random number statement

--- src/dmbelov/B1.java
+++ src/dmbelov/B1.java
@@ -3,10 +3,10 @@ package dmbelov;
 public class B1 {
     public static void main(String[] args) {
 
-        int randomNumber = (int) ((Math.random()+4)* 31);
+        int randomNumber = (int) (Math.random()*151+5);
         String statement = randomNumber >= 25 && randomNumber <= 100 ?
-                randomNumber + " Число попало в интервал [25;100]" :
-                randomNumber + " Число НЕ попало в интервал [25;100]";
+                randomNumber + " The number is in interval [25;100]" :
+                randomNumber + " The number is not in interval [25;100]";
 
         System.out.println(statement);
     }
