6197f55272fef70dc0e639e29a5b6f5b60fd0a9e
NatalliaRmn <natalliaremeniako@gmail.com>
2019-06-01 03:12:47 -0800

    task A lesson2_Update

---
+++ src/nremeniako/lesson2/TaskB3.java
@@ -0,0 +1,18 @@
+package nremeniako.lesson2;
+
+import java.util.Scanner;
+
+public class TaskB3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("будьте так любезны ввести любое положительное число");
+
+        int a = scanner.nextInt();
+        int sum=0;
+
+        for (int i=1;i<=a;i++){
+            sum=sum+i;
+        }{
+        int z=sum;
+        System.out.println("Получите:" + " "+ z);
+        }   } }
