a0cc1c6f79265d00b7692d2bee58f3a551fc7bb6
NatalliaRmn <natalliaremeniako@gmail.com>
2019-06-03 09:32:02 -0800

    task A lesson2_Update

---
+++ src/nremeniako/lesson3/task2.java
@@ -0,0 +1,24 @@
+package nremeniako.lesson3;
+
+public class task2 {
+
+        public static void main(String args[]){
+
+            int sum = 2;
+
+            int sum2 = 0;
+
+            for (int i = sum; i <= 19;i++ ) {
+
+                sum *=2;
+
+                sum2++;
+
+                System.out.println(sum2 + " элемент - " + sum);
+            }
+
+        }
+}
+
+
+
