1fbb67973deae96fdbdd25279a1d9717043f243e
dmbelov <dmbelov777@gmail.com>
2019-06-03 21:03:11 +0300

    Lesson3 Tasks A5,A6 done

---
+++ src/dmbelov/Lesson3/A6.java
@@ -0,0 +1,16 @@
+package dmbelov.Lesson3;
+
+public class A6 {
+    public static void main(String[] args) {
+
+        int a=-166;
+
+        for(int i=1;a<100;i++){
+              if (a>-100) {
+                System.out.println(a);
+            }
+            a=2*a-1+200;
+        }
+
+    }
+}
