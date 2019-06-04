80bc0b1cfcea088ffe0f1a6f2e2d11026dacfa9a
DonBirnam <ivan.zhuk94@gmail.ru>
2019-06-04 23:59:24 +0300

    Completed tasks from Lesson 2 Completed tasks from Lesson 3 (exept task B2)

---
+++ src/izhuk/Lesson3/lesson3A5.java
@@ -0,0 +1,10 @@
+package izhuk.Lesson3;
+
+public class lesson3A5 {
+    public static void main(String[] args){
+        for (int a = 2;a < 10000;a =2*a - 1){
+            System.out.print(a + " ");
+        }
+    }
+}
+
