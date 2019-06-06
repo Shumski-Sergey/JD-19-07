9f29bc7aadf79da376184560cbd4f62f956542b7
ralmnsk <ralmnsk@gmail.com>
2019-06-06 09:04:23 +0300

    Leson4 all

--- src/rashkevich/lesson4/TaskB2.java
+++ src/rashkevich/lesson4/TaskB2.java
@@ -1,19 +1,33 @@
 package rashkevich.lesson4;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskB2 {
     public static void main(String[] args){
-        String text="Process finished with exit code 0";
+        String text="prohijkcess finished with abcd exit code 0";
         String text2=text.replace(" ","");
         System.out.println(text2);
         char chars[]=text2.toCharArray();
-        for (int i=0; i<chars.length;i++){
-            String posled="";
-            for (int j=i+1;i<chars.length;j++){
-                if (chars[j-1]==chars[j]){
-                    posled=posled+chars[j];
+        for (int i=0;i<chars.length;i++){
+            char ch=chars[i];
+            String str=""+ch;
+            String regex="\\s*"+str+"\\s*";
+                for (int j=i;j<chars.length;j++){
+                    if((int)(ch)<(int)('z')){
+                        ch=(char)((int)ch+1);
+                        regex=regex+ch+"\\s*";
+
+                        Pattern p=Pattern.compile(regex);
+                        Matcher m=p.matcher(text);
+
+                        if(m.find()){
+                            System.out.println(m.group(0));
+                        }
+
+                    }
+
                 }
-            }
-            System.out.println(posled);
         }
     }
 }
