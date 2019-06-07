9294ad7e9c1a79969504d4b51fbcee8097175524
Сергей <s.guess1973@gmail.com>
2019-06-07 22:04:27 +0300

    Merge branch 'master' of D:\JavaProject\Tasks with conflicts.

--- src/ezhalabkevich/L2/B2.java
+++ src/ezhalabkevich/L2/B2.java
@@ -2,7 +2,7 @@ package ezhalabkevich.L2;
 
 public class B2 {
     public static void main(String[] args) {
-        // Task B2
+        // Task poker
         for (int i = 5; i > 0; i--) {
             System.out.print(" " + i);
         }
