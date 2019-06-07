010749384324dbcfc8a641a331b9ed30dd31c64d
ralmnsk <ralmnsk@gmail.com>
2019-06-07 21:17:48 +0300

    Leson5 test1

---
+++ src/rashkevich/lesson5/ClassOne.java
@@ -0,0 +1,13 @@
+package rashkevich.lesson5;
+
+public class ClassOne {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public ClassOne(String string) {
+        name = string;
+    }
+}
