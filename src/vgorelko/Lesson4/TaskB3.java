558c848a778a7016003a73e51fbc7724c5d95a3e
Gorelko90 <gorelko90@gmail.com>
2019-06-07 20:37:17 +0300

    Lesson3

--- src/vgorelko/Lesson4/TaskB3.java
+++ src/vgorelko/Lesson4/TaskB3.java
@@ -1,14 +1,24 @@
 package vgorelko.Lesson4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskB3 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-    }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String adress  = reader.readLine();
 
-    static {
+        Pattern pattern = Pattern.compile("https?:\\/\\/[a-z0-9]+\\.[a-z]+\\/?");
+        Matcher matcher = pattern.matcher(adress);
 
-        System.out.println("Hello");
+        System.out.println(matcher.find());
+
+
 
     }
 
