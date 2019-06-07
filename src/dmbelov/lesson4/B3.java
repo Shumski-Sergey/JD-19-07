e60970baa87816f812f11b6c3c3c4fa9388f288b
dmbelov <dmbelov777@gmail.com>
2019-06-07 21:15:39 +0300

    Lesson4 Tasks B3 is done

---
+++ src/dmbelov/lesson4/B3.java
@@ -0,0 +1,18 @@
+package dmbelov.lesson4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class B3 {
+    public static void main(String[] args) {
+
+        String string = "http://example.com";
+
+        Pattern pattern = Pattern.compile("^((http|https)(://)[a-zA-Z0-9.-]+)$");
+        Matcher matcher = pattern.matcher(string);
+
+        while(matcher.find()) {
+            System.out.println(string.substring(matcher.start(), matcher.end()));
+        }
+    }
+}
