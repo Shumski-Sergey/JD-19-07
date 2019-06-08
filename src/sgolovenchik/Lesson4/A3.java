1c412cdbeca58d27a86921a8d01c8dae53542faf
Сергей <s.guess1973@gmail.com>
2019-06-08 18:08:44 +0300

    lesson4

--- src/sgolovenchik/Lesson4/A3.java
+++ src/sgolovenchik/Lesson4/A3.java
@@ -2,21 +2,20 @@ package sgolovenchik.Lesson4;
 
 import java.util.Scanner;
 
-public class A2 {
+public class A3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите строку: ");
         String text = in.nextLine();
         char[] textArray = text.toCharArray();
-        int count = 0;
         for (int i = 0; i < textArray.length; i++) {
             if (textArray[i] != ' ') {
                 while (textArray[i] != ' ') {
                     i++; if ( i == textArray.length ) {break;}
                 }
-                count++;
+                System.out.print(textArray[i-1]);
             }
         }
-        System.out.println("В строке " +count+ " слов.");
     }
 }
+
