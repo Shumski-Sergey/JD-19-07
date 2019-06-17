c71f444b4c1cca334f5bd9f474b2d8b504a67f4a
tolifer <tanya.olifer96@gmail.com>
2019-06-17 15:28:27 +0300

    lesson 7

---
+++ src/tolifer/L7/aboutMoney/Service.java
@@ -0,0 +1,17 @@
+package tolifer.L7.aboutMoney;
+
+public class Service {
+    public static double addition(double a, double b){
+        return a+b;
+    }
+
+    public static double subtraction(double a, double b){
+        return a-b;
+    }
+    public static  double division (double a, double b){
+        return a/b;
+    }
+    public static double multiplication (double a, double b){
+        return a*b;
+    }
+}
