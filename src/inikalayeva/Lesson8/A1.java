70408a73659251244bf2239269711aa9a376c251
smthyellow <nickol.husky@gmail.com>
2019-06-19 15:06:48 +0300

    Lesson8 A1

--- src/inikalayeva/Lesson8/A1.java
+++ src/inikalayeva/Lesson8/A1.java
@@ -1,4 +1,17 @@
 package src.inikalayeva.Lesson8;
 
-public class A1<T, arr> {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class A1 <T>{
+    private static T[] arrayForTask;
+    T[] arrayForTask = {2.3, 7.5, 3.6, 7.4, 5.9};
+    static T giveMeElement( T [] arrayForTask, int i) {
+        return arrayForTask[i];
+    }
+
+    public static void main (String[] args){
+        System.out.println(giveMeElement(arrayForTask, 2));
+    }
+
 }
