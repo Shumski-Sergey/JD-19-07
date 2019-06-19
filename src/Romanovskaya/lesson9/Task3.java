eb1b579d9c1458279bbd7477f845c298a4c23f1c
Nastyaromanovsk <nastyaromanovskaya55@gmail.com>
2019-06-19 21:00:03 +0300

    lesson8-9

---
+++ src/Romanovskaya/lesson9/Task3.java
@@ -0,0 +1,21 @@
+package romanovskaya.lesson9;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Task3 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(2);
+        list.add(5);
+        list.add(10);
+        list.add(8);
+        list.add(3);
+        list.add(6);
+
+        System.out.println("Список всех оценок");
+
+        System.out.println(list);
+    }
+}
