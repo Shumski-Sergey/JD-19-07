0486112db300a7dfdf116b5d09359775fb780a92
tolifer <tanya.olifer96@gmail.com>
2019-06-26 18:35:04 +0300

    Lesson5

--- src/tolifer/zad1.java
+++ src/tolifer/zad1.java
@@ -4,7 +4,6 @@ public class zad1 {
     public static void main (String [] args){
         double m = 15;
         double n = 1;
-        //////
 
         if (10%m < 10%n){
             System.out.println(m);
