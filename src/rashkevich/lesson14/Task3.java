12012302a35d0de466635e2229946127a3dc0d3e
ralmnsk <ralmnsk@gmail.com>
2019-07-05 18:13:14 +0300

    Lesson14

--- src/rashkevich/lesson14/Task3.java
+++ src/rashkevich/lesson14/Task3.java
@@ -1,34 +1,53 @@
-package rashkevich.lesson14;
 
 //3.Организовать построчное считывание данных, введенных с консоли,
 //        и их запись в файл. Признаком окончания записи (конец работы программы)
 //        является строка со словом «#ESC».
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args){
-        String str=new String("");
-        String filename="C:\\Users\\iland\\IdeaProjects\\JD-19-07_clone5\\src\\rashkevich\\lesson14\\out3.txt";
-        File file = new File(filename);
-        try (BufferedWriter bw=new BufferedWriter(new FileWriter(file));){
-
-        while (str!="#ESC"){
-            System.out.println("Введите число:");
-            Scanner sc = new Scanner(System.in);
+        LinkedList<String> list=input();
+        writeInFile(list);
+    }
+    public static LinkedList<String> input(){
+        LinkedList<String> list=new LinkedList<String>();
+        String str="";
+        Scanner sc=new Scanner(System.in);
+        while(str!="#ESC"){
             str=sc.nextLine();
             System.out.println(str);
-            if (str!="#ESC"){
-            bw.write(str);
+            if (str.equals("#ESC")){
+                sc.close();
+                break;
             }
+            list.add(str);
         }
+        return list;
+    }
+
+    public static void writeInFile(List<String> list){
+        String filename="out.txt";
+        File file=new File(filename);
+        PrintWriter fr=null;
+        try {
+            fr=new PrintWriter(file.getAbsolutePath());
+            System.out.println("File path:"+file.getAbsolutePath());
+            for (String str:list){
+                fr.println(str);
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            if (fr!=null){
+                fr.close();
+            }
         }
-
     }
 }
---
+++ src/rashkevich/lesson14/Task4.java
@@ -0,0 +1,56 @@
+//B1. Считать из одного файла текст НА РУССКОМ и НА АНГЛ.
+//        и вывести в другой. Нагенерить(создать) файл минимум 100мб.
+//        В репозиторий GIT не закидывать!!!!
+
+import java.io.*;
+import java.nio.channels.FileChannel;
+
+public class Task4 {
+    public static void main(String[] args){
+        String fname="src/text.txt";
+        String fnameOut="src/outtext.txt";
+        File f=new File(fname);
+        File fout=new File(fnameOut);
+        System.out.println(f.getAbsolutePath()+"---"+fout.getAbsolutePath());
+        String str=new String();
+
+        BufferedReader br=null;
+        BufferedWriter bw=null;
+        int size=1024*1000*100;
+        try {
+             br=new BufferedReader(new InputStreamReader(new FileInputStream(f.getAbsolutePath()),"Cp1251"));
+             bw=new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fout.getAbsolutePath()),"Cp1251"));
+             while (fout.length()<size){
+                br.mark(size);
+             while((str=br.readLine())!=null){
+                str=str;
+                bw.append(str);
+                bw.newLine();
+             }
+                br.reset();
+             }
+
+            System.out.println(fout.length());
+//            FileChannel in = new RandomAccessFile("src/text.txt", "r").getChannel();
+//            FileChannel out=new RandomAccessFile("src/outtext.txt","rw").getChannel();
+//            out.transferFrom(in,0,Long.MAX_VALUE);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br!=null){
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bw!=null){
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
